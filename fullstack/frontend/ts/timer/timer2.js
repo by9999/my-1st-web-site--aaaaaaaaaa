630cc0e63567114c7030838a07736ccd1fc09362
by9999 <learnbathula@gmail.com>
2022-12-21 13:05:59 +0530

    game

---
+++ fullstack/frontend/ts/timer/timer2.js
@@ -0,0 +1,10 @@
+var Main = /** @class */ (function () {
+    function Main() {
+    }
+    Main.prototype.main = function (String, _a, args) {
+        LocalDate;
+        myObj = LocalDate.now(); // Create a date object
+        System.out.println(myObj); // Display the current date
+    };
+    return Main;
+}());
